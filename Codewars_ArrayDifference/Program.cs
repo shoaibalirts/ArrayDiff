56b476d22fb6ed8993d4d144d050b066fe49216a
shoaibalirts <shoaibali.rts@gmail.com>
2026-04-29 20:21:07 +0200

    Add project files.

---
+++ Codewars_ArrayDifference/Program.cs
@@ -0,0 +1,51 @@
+﻿
+Console.WriteLine("Codewars: Computing Difference between arrays !");
+//int[] a = new int[2] {1, 2}; // {1, 2, 2, 2, 3}
+//int[] b = new int[1] {1};    // {1, 3}
+
+int[] a = new int[5] {1, 2, 2, 2, 3};
+int[] b = new int[1] {2};
+int count = 0;
+for(int i=0; i < a.Length; i++)
+{
+    bool isFound = false;
+    for(int j=0;j< b.Length; j++)// 
+    {
+        if (a[i] == b[j])
+        {
+            isFound = true;
+            break;
+        }
+    }
+    if (isFound == false)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Number of elements Not Matched: {count}");
+int currentIndex = 0;
+int[] result = new int[count];
+for (int i = 0; i < a.Length; i++)
+{
+    bool isFound = false;
+    for (int j = 0; j < b.Length; j++)// 
+    {
+        if (a[i] == b[j])
+        {
+            isFound = true;
+            break;
+        }
+    }
+    if (isFound == false)
+    {
+        result[currentIndex] = a[i];
+        currentIndex++;
+    }
+}
+
+//Console.WriteLine(result); 
+
+
+    Console.WriteLine(string.Join(", ", result));
+Console.ReadKey();
